2e401aabcc081d8ca8be8569d021240c5d1f44a0
Thitiya0901 <66010914026@msu.ac.th>
2026-01-28 16:23:16 +0700

    a

--- g/connectdb.php
+++ g/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pwd  = "";
+$pwd  = "@1458800032693";
 $db   = "4026db";
 $conn = mysqli_connect($host, $user, $pwd, $db)
         or die("เชื่อมต่อฐานข้อมูลไม่ได้");
