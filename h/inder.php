da35cc31bd47e5b47331b58252e6000e184e1e57
Thitiya0901 <66010914026@msu.ac.th>
2026-02-04 14:39:11 +0700

    i

---
+++ h/inder.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ฐิติยา พุทธลา (พรีม)</title>
+</head>
+
+<body>
+<h1>เข้าสู่ระบบ - ฐิติยา</h1>
+
+<form method="post"action="">
+Username<input type="text"name="auser" autofocus required><br>
+password<input type="password" name="apwd"required><br>
+<button type="submit" name="Submit">LOGIN</button>
+</form>
+
+</body>
+</html>
