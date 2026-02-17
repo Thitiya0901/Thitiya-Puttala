a93e4867c4a422de5803eba7899212ff242b47d9
Thitiya-Puttala <66010914026@msu.ac.th>
2026-02-17 19:49:29 +0700

    kopk

---
+++ i/connectdb.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pwd  = "Pw@1458800032693";
+$db   = "4026db";
+$conn = mysqli_connect($host, $user, $pwd, $db)
+        or die("เชื่อมต่อฐานข้อมูลไม่ได้");
+mysqli_set_charset($conn, "utf8");
+?>
