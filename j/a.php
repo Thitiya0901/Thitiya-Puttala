18b96aea548e32441998899d95671a4f45bf4600
Thitiya-Puttala <66010914026@msu.ac.th>
2026-02-18 14:11:03 +0700

    aa

---
+++ j/a.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ฐิติยา พุทธลา (พรีม)</title>
+</head>
+
+<body>
+
+<h1>66010914026 ฐิติยา พุทธลา (พรีม)</h1>
+<br>
+<img src="images/1.jpg"width="400">
+
+
+</body>
+</html>
